a29d79a691d5504f38607189063bfe53bade28c5
Yi Zuo <zuoyi@bu.edu>
2020-11-17 20:40:23 -0600

    rearrange files

--- R/pro.sgpv.R
+++ R/pro.sgpv.R
@@ -5,8 +5,9 @@
 #'
 #' @importFrom stats complete.cases coef lm predict
 #' @importFrom glmnet cv.glmnet
+#' @importFrom graphics abline axis lines mtext polygon
 #' @param x Independent variables, can be a \code{matrix} or a \code{data.frame}
-#' @param y Dependent variable, can be a \code{vector} or a column from a \code{data.frame}
+#' @param y Dependent variable, can be a 'vector' or a column from a \code{data.frame}
 #' @param stage Algorithm indicator. 1 denotes the one-stage algorithm and
 #' 2 denotes the two-stage algorithm. Default is 1.
 #'
@@ -21,7 +22,7 @@
 #' @export
 #'
 #' @examples
-#' # more examples at https://github.com/zuoyi93/ProSGPV
+#' # Examples at https://github.com/zuoyi93/ProSGPV
 
 pro.sgpv <- function(x, y, stage=1){
 
@@ -62,6 +63,194 @@ pro.sgpv <- function(x, y, stage=1){
 
   class(out) <- "sgpv"
   return(out)
+
+}
+
+
+#' @export
+print.sgpv <- function(x,...){
+  if(length(x$var.index)>0){
+    cat("Selected variables are", x$var.label)
+  }else cat("None of variables are selected.")
+
+}
+
+
+#' @export
+coef.sgpv <- function(object,...){
+
+  if(length(object$var.index)>0){
+    lm.d <- data.frame(yy=object$y,xx = object$x[,object$var.index])
+    colnames(lm.d)[-1] <- object$var.label
+    coef(lm(yy~.,data=lm.d))
+  }else stop("None of variables are selected.")
+
+}
+
+#' @export
+summary.sgpv <- function(object,...){
+
+  if(length(object$var.index)>0){
+    lm.d <- data.frame(yy=object$y,xx = object$x[,object$var.index])
+    colnames(lm.d)[1] <- "Response"
+    colnames(lm.d)[-1] <- object$var.label
+    summary(lm(Response~.,data=lm.d))
+  }else stop("None of variables are selected.")
+
+}
+
+#' @export
+predict.sgpv <- function(object,newx,...){
+
+  if(length(object$var.index)>0){
+    lm.d <- data.frame(yy=object$y,xx = object$x[,object$var.index])
+    colnames(lm.d)[-1] <- object$var.label
+    lm.m <- lm(yy~.,data=lm.d)
+
+    if(missing(newx)) newx <- object$x
+
+    predict(lm.m,newx)
+  }else stop("None of variables are selected and prediction is not available.")
+
+}
+
+#' @export
+plot.sgpv <- function(x,lpv=3,lambda.max=NULL,...){
+
+  if(is.null(x$lambda)) stop("One-stage algorithm doesn't have the plot function.")
+  if(! lpv %in% c(1,3)) stop("lpv argument only takes values of 1 and 3.")
+
+  # get information from data
+  p <- ncol(x$x)
+  x.names <- colnames(x$x)
+
+  # standardize data
+  xs <- scale(x$x)
+  ys <- scale(x$y)
+
+  # maximum lambda
+  if(is.null(lambda.max)) lambda.max <- max(abs(sapply(1:ncol(xs),function(z) xs[,z] %*% ys ))/nrow(xs))*1.1
+
+  step = lambda.max/100
+
+  # get a sequence of lambda
+  lambda.seq <- seq(0,lambda.max,step)
+
+  # get coefficient estimates at each lambda
+  results <- sapply(lambda.seq,function(z) get.coef(xs=xs,ys=ys,lambda=z))
+
+  # prepare data to plot
+  to.plot <- data.frame(lambda=rep(lambda.seq,each=p),
+                        v=rep(x.names,length(lambda.seq)),
+                        pe=c(results[1:p,]),
+                        lb=c(results[(p+1):(2*p),]),
+                        ub=c(results[(2*p+1):(3*p),])
+  )
+
+  if(lpv==1){
+    to.plot$bound <- ifelse(abs(to.plot$lb)<abs(to.plot$ub), to.plot$lb, to.plot$ub )
+    plot.d <- to.plot[,c("lambda","v","bound")]
+
+    # find the location of x.names
+    location.beta <- to.plot$bound[to.plot$lambda==0]
+
+  }else if(lpv==3){
+    plot.d <- to.plot
+
+    # find the location of x.names
+    location.beta <- to.plot$pe[to.plot$lambda==0]
+  }
+
+  # get the indices of selected variables
+  selected.index <- x$var.index
+  black.index <- setdiff(1:p,selected.index)
+
+  # change the color of the variables
+  color.use <- c(rep("black",length(black.index)),
+                 rep("blue",length(selected.index)))
+
+  location.beta <- location.beta[c(black.index,selected.index)]
+  var.axis <- x.names[c(black.index,selected.index)]
+
+  # find the limit of the canvas
+  if(lpv==3){
+    ylim <- c(min(c(location.beta,to.plot$lb,to.plot$ub) )*1.01,
+              max(c(location.beta,to.plot$lb,to.plot$ub))*1.01)
+
+    ylim <- c(-max(abs(ylim)),max(abs(ylim)))
+    ytick <- c(-round(max(abs(ylim)),2),-round(max(abs(ylim)),2)/2,0,
+               round(max(abs(ylim)),2)/2,round(max(abs(ylim)),2) )
+  }else if (lpv==1){
+    ylim <- c(min(plot.d$bound) * 1.01,
+              max(plot.d$bound) * 1.01)
+
+    ylim <- c(-max(abs(ylim)),max(abs(ylim)))
+    ytick <- c(-round(max(abs(ylim)),2),-round(max(abs(ylim)),2)/2,0,
+               round(max(abs(ylim)),2)/2,round(max(abs(ylim)),2) )
+
+  }
+
+  # create a data set for the null bound
+  n.bound <- results[(3*p+1),]
+
+  # find the lambda.1se
+  vlambda <- x$lambda
+
+  # plot the results
+  if(lpv==1){
+
+    xvals <- split(plot.d$lambda,plot.d$v)
+    yvals <- split(plot.d$bound,plot.d$v)
+
+    plot(lambda.seq,type="n",ylim=ylim,xlim=c(0,lambda.max),
+         xlab=expression(lambda),ylab="Bound that is closer to the null",
+         main="Solution to the two-stage algorithm with one line per variable",
+         axes=F,frame.plot=T)
+    axis(1,at=round(seq(0,lambda.max,length.out=5),3) )
+    axis(2,at=ytick,labels=c(ytick[1:2],0,ytick[4:5]))
+    mtext(var.axis,side=2,at=location.beta,col=color.use)
+    mtext(bquote(lambda["1se"]),side=1,at=vlambda)
+    polygon(c(lambda.seq,rev(lambda.seq)),c(-n.bound,rev(n.bound)),col="grey",border="grey")
+    invisible(mapply(lines,xvals,yvals,col=1:p))
+    abline(h=0)
+    abline(v=vlambda,lty=2)
+
+
+  }else if (lpv==3){
+
+    plot(lambda.seq,type="n",ylim=ylim,xlim=c(0,lambda.max),
+         xlab=expression(lambda),ylab="Point estimates and confidence intervals",
+         main="Solution to the two-stage algorithm with three lines per variable",
+         axes=F,frame.plot=T)
+    axis(1,at=round(seq(0,lambda.max,length.out=5),3) )
+    axis(2,at=ytick,labels=c(ytick[1:2],0,ytick[4:5]))
+
+    # null region
+    polygon(c(lambda.seq,rev(lambda.seq)),c(-n.bound,rev(n.bound)),col="grey",border="grey")
+
+    # text on axis
+    mtext(var.axis,side=2,at=location.beta,col=color.use)
+    mtext(bquote(lambda["1se"]),side=1,at=vlambda)
+
+    # point estimates
+    xvals <- split(plot.d$lambda,plot.d$v)
+    yvals <- split(plot.d$pe,plot.d$v)
+    invisible(mapply(lines,xvals,yvals,col=1:p))
+
+    # upper bound
+    xvals <- split(plot.d$lambda,plot.d$v)
+    yvals <- split(plot.d$ub,plot.d$v)
+    invisible(mapply(lines,xvals,yvals,col=1:p,lty=2))
+
+    # lower bound
+    xvals <- split(plot.d$lambda,plot.d$v)
+    yvals <- split(plot.d$lb,plot.d$v)
+    invisible(mapply(lines,xvals,yvals,col=1:p,lty=2))
+
+    abline(h=0)
+    abline(v=vlambda,lty=2)
+
+  }
 
 }
 
--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,38 @@
-#' get.coef function
+#' Get indices
+#'
+#' Get the indices of the variables selected by the algorithm
+#'
+#' @importFrom stats lm
+#' @param candidate.index Indices of the candidate set
+#' @param xs Standardized independent variables
+#' @param ys Standardized dependent variable
+#'
+#' @return Indices of variables selected
+#' @export
+
+get.var <- function(candidate.index,xs,ys){
+
+  if(length(candidate.index)==0){
+    out.sgpv <- integer(0)
+  }else{
+
+    # run fully relaxed LASSO
+    f.l <- lm(ys~xs[,candidate.index])
+
+    # get confidence bands
+    pe <- summary(f.l)$coef[-1,1]
+    se <- summary(f.l)$coef[-1,2]
+    null.bound.p <- mean(se)
+
+    # screen variables
+    out.sgpv <- candidate.index[which(abs(pe)> 1.96*se+null.bound.p )]
+  }
+
+  return( out.sgpv)
+}
+
+
+#' Get coefficients at each \code{lambda}
 #'
 #' Get the coefficients and confidence intervals from regression at each \code{lambda}
 #' as well as the null bound in SGPVs
