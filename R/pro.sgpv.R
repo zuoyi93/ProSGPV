a82d7520d1d9a343da8cee3721846a9bc158cc7f
Yi Zuo <zuoyi@bu.edu>
2020-11-19 19:47:22 -0600

    style the code

--- R/pro.sgpv.R
+++ R/pro.sgpv.R
@@ -31,54 +31,53 @@
 #' library(ProSGPV)
 #'
 #' # prepare the data
-#' x = t.housing[,-ncol(t.housing)]
-#' y = t.housing$V9
+#' x <- t.housing[, -ncol(t.housing)]
+#' y <- t.housing$V9
 #'
 #' # run one-stage algorithm
 #' out.sgpv.1 <- pro.sgpv(x = x, y = y, stage = 1)
 #'
 #' # More examples at https://github.com/zuoyi93/ProSGPV
+pro.sgpv <- function(x, y, stage = 1) {
+  if (!(stage %in% 1:2)) stop("Stage only takes value of 1 or 2.")
 
-pro.sgpv <- function(x, y, stage=1){
+  if (nrow(x) != length(y)) stop("Input x and y have different number of observations")
 
-  if(!(stage%in% 1:2)) stop("Stage only takes value of 1 or 2.")
+  if (!is.numeric(as.matrix(x)) | !is.numeric(y)) stop("The input data have non-numeric values.")
 
-  if(nrow(x)!= length(y)) stop("Input x and y have different number of observations")
-
-  if(!is.numeric(as.matrix(x)) | !is.numeric(y)) stop("The input data have non-numeric values.")
-
-  if(any(complete.cases(x)==F) | any(complete.cases(y)==F)){
+  if (any(complete.cases(x) == F) | any(complete.cases(y) == F)) {
     warning("Only complete records will be used.")
-    comp.index <- complete.cases(data.frame(x,y))
-    x <- x[comp.index,]
+    comp.index <- complete.cases(data.frame(x, y))
+    x <- x[comp.index, ]
     y <- y[comp.index]
   }
 
-  if(is.null(colnames(x))) colnames(x) <- paste("V",1:ncol(x),sep="")
+  if (is.null(colnames(x))) colnames(x) <- paste("V", 1:ncol(x), sep = "")
 
   xs <- scale(x)
   ys <- scale(y)
 
-  if(stage == 2){
-    lasso.cv <- cv.glmnet(xs,ys)
-    lambda = lasso.cv$lambda.1se
-    candidate.index <- which(coef(lasso.cv,s=lambda)[-1] != 0)
-  }else{
+  if (stage == 2) {
+    lasso.cv <- cv.glmnet(xs, ys)
+    lambda <- lasso.cv$lambda.1se
+    candidate.index <- which(coef(lasso.cv, s = lambda)[-1] != 0)
+  } else {
     candidate.index <- 1:ncol(xs)
-    lambda = NULL
+    lambda <- NULL
   }
 
   out.sgpv <- get.var(candidate.index, xs, ys)
 
-  out <- list(var.index=out.sgpv,
-       var.label=colnames(x)[out.sgpv],
-       lambda=lambda,
-       x=x,
-       y=y)
+  out <- list(
+    var.index = out.sgpv,
+    var.label = colnames(x)[out.sgpv],
+    lambda = lambda,
+    x = x,
+    y = y
+  )
 
   class(out) <- "sgpv"
   return(out)
-
 }
 
 #' Print variable selection results
@@ -96,19 +95,19 @@ pro.sgpv <- function(x, y, stage=1){
 #' library(ProSGPV)
 #'
 #' # prepare the data
-#' x = t.housing[,-ncol(t.housing)]
-#' y = t.housing$V9
+#' x <- t.housing[, -ncol(t.housing)]
+#' y <- t.housing$V9
 #'
 #' # run one-stage algorithm
 #' out.sgpv.1 <- pro.sgpv(x = x, y = y, stage = 1)
 #'
 #' out.sgpv.1
-
-print.sgpv <- function(x,...){
-  if(length(x$var.index)>0){
+print.sgpv <- function(x, ...) {
+  if (length(x$var.index) > 0) {
     cat("Selected variables are", x$var.label)
-  }else cat("None of variables are selected.")
-
+  } else {
+    cat("None of variables are selected.")
+  }
 }
 
 #' Extract coefficients from the model fit
@@ -127,23 +126,22 @@ print.sgpv <- function(x,...){
 #' library(ProSGPV)
 #'
 #' # prepare the data
-#' x = t.housing[,-ncol(t.housing)]
-#' y = t.housing$V9
+#' x <- t.housing[, -ncol(t.housing)]
+#' y <- t.housing$V9
 #'
 #' # run one-stage algorithm
 #' out.sgpv.1 <- pro.sgpv(x = x, y = y, stage = 1)
 #'
 #' # get coefficients
 #' coef(out.sgpv.1)
-
-coef.sgpv <- function(object,...){
-
-  if(length(object$var.index)>0){
-    lm.d <- data.frame(yy=object$y,xx = object$x[,object$var.index])
+coef.sgpv <- function(object, ...) {
+  if (length(object$var.index) > 0) {
+    lm.d <- data.frame(yy = object$y, xx = object$x[, object$var.index])
     colnames(lm.d)[-1] <- object$var.label
-    coef(lm(yy~.,data=lm.d))
-  }else stop("None of variables are selected.")
-
+    coef(lm(yy ~ ., data = lm.d))
+  } else {
+    stop("None of variables are selected.")
+  }
 }
 
 #' Summary of the OLS model on selected variables
@@ -161,24 +159,23 @@ coef.sgpv <- function(object,...){
 #' library(ProSGPV)
 #'
 #' # prepare the data
-#' x = t.housing[,-ncol(t.housing)]
-#' y = t.housing$V9
+#' x <- t.housing[, -ncol(t.housing)]
+#' y <- t.housing$V9
 #'
 #' # run one-stage algorithm
 #' out.sgpv.1 <- pro.sgpv(x = x, y = y, stage = 1)
 #'
 #' # get regression summary
 #' summary(out.sgpv.1)
-
-summary.sgpv <- function(object,...){
-
-  if(length(object$var.index)>0){
-    lm.d <- data.frame(yy=object$y,xx = object$x[,object$var.index])
+summary.sgpv <- function(object, ...) {
+  if (length(object$var.index) > 0) {
+    lm.d <- data.frame(yy = object$y, xx = object$x[, object$var.index])
     colnames(lm.d)[1] <- "Response"
     colnames(lm.d)[-1] <- object$var.label
-    summary(lm(Response~.,data=lm.d))
-  }else stop("None of variables are selected.")
-
+    summary(lm(Response ~ ., data = lm.d))
+  } else {
+    stop("None of variables are selected.")
+  }
 }
 
 #' Prediction using the fitted model
@@ -198,26 +195,25 @@ summary.sgpv <- function(object,...){
 #' library(ProSGPV)
 #'
 #' # prepare the data
-#' x = t.housing[,-ncol(t.housing)]
-#' y = t.housing$V9
+#' x <- t.housing[, -ncol(t.housing)]
+#' y <- t.housing$V9
 #'
 #' # run one-stage algorithm
 #' out.sgpv.1 <- pro.sgpv(x = x, y = y, stage = 1)
 #'
 #' predict(out.sgpv.1)
-
-predict.sgpv <- function(object,newx,...){
-
-  if(length(object$var.index)>0){
-    lm.d <- data.frame(yy=object$y,xx = object$x[,object$var.index])
+predict.sgpv <- function(object, newx, ...) {
+  if (length(object$var.index) > 0) {
+    lm.d <- data.frame(yy = object$y, xx = object$x[, object$var.index])
     colnames(lm.d)[-1] <- object$var.label
-    lm.m <- lm(yy~.,data=lm.d)
+    lm.m <- lm(yy ~ ., data = lm.d)
 
-    if(missing(newx)) newx <- object$x
+    if (missing(newx)) newx <- object$x
 
-    predict(lm.m,newx)
-  }else stop("None of variables are selected and prediction is not available.")
-
+    predict(lm.m, newx)
+  } else {
+    stop("None of variables are selected and prediction is not available.")
+  }
 }
 
 #' Plot variable selection results
@@ -242,8 +238,8 @@ predict.sgpv <- function(object,newx,...){
 #' library(ProSGPV)
 #'
 #' # prepare the data
-#' x = t.housing[,-ncol(t.housing)]
-#' y = t.housing$V9
+#' x <- t.housing[, -ncol(t.housing)]
+#' y <- t.housing$V9
 #'
 #' # two-stage algorithm
 #' out.sgpv.2 <- pro.sgpv(x = x, y = y, stage = 2)
@@ -252,15 +248,13 @@ predict.sgpv <- function(object,newx,...){
 #' plot(out.sgpv.2)
 #'
 #' # zoom in a little bit
-#' plot(out.sgpv.2,lambda.max=0.01)
+#' plot(out.sgpv.2, lambda.max = 0.01)
 #'
 #' # only plot one confidence bound
-#' plot(out.sgpv.2,lpv=1,lambda.max=0.01)
-
-plot.sgpv <- function(x,lpv=3,lambda.max=NULL,...){
-
-  if(is.null(x$lambda)) stop("One-stage algorithm doesn't have the plot function.")
-  if(! lpv %in% c(1,3)) stop("lpv argument only takes values of 1 and 3.")
+#' plot(out.sgpv.2, lpv = 1, lambda.max = 0.01)
+plot.sgpv <- function(x, lpv = 3, lambda.max = NULL, ...) {
+  if (is.null(x$lambda)) stop("One-stage algorithm doesn't have the plot function.")
+  if (!lpv %in% c(1, 3)) stop("lpv argument only takes values of 1 and 3.")
 
   # get information from data
   p <- ncol(x$x)
@@ -271,131 +265,137 @@ plot.sgpv <- function(x,lpv=3,lambda.max=NULL,...){
   ys <- scale(x$y)
 
   # maximum lambda
-  if(is.null(lambda.max)) lambda.max <- max(abs(sapply(1:ncol(xs),function(z) xs[,z] %*% ys ))/nrow(xs))*1.1
+  if (is.null(lambda.max)) lambda.max <- max(abs(sapply(1:ncol(xs), function(z) xs[, z] %*% ys)) / nrow(xs)) * 1.1
 
-  step = lambda.max/100
+  step <- lambda.max / 100
 
   # get a sequence of lambda
-  lambda.seq <- seq(0,lambda.max,step)
+  lambda.seq <- seq(0, lambda.max, step)
 
   # fit lasso once
-  lasso <- glmnet(xs,ys)
+  lasso <- glmnet(xs, ys)
 
   # get coefficient estimates at each lambda
-  results <- sapply(lambda.seq,function(z) get.coef(xs=xs,ys=ys,lambda=z,lasso=lasso))
+  results <- sapply(lambda.seq, function(z) get.coef(xs = xs, ys = ys, lambda = z, lasso = lasso))
 
   # prepare data to plot
-  to.plot <- data.frame(lambda=rep(lambda.seq,each=p),
-                        v=rep(x.names,length(lambda.seq)),
-                        pe=c(results[1:p,]),
-                        lb=c(results[(p+1):(2*p),]),
-                        ub=c(results[(2*p+1):(3*p),])
+  to.plot <- data.frame(
+    lambda = rep(lambda.seq, each = p),
+    v = rep(x.names, length(lambda.seq)),
+    pe = c(results[1:p, ]),
+    lb = c(results[(p + 1):(2 * p), ]),
+    ub = c(results[(2 * p + 1):(3 * p), ])
   )
 
-  if(lpv==1){
-    to.plot$bound <- ifelse(abs(to.plot$lb)<abs(to.plot$ub), to.plot$lb, to.plot$ub )
-    plot.d <- to.plot[,c("lambda","v","bound")]
+  if (lpv == 1) {
+    to.plot$bound <- ifelse(abs(to.plot$lb) < abs(to.plot$ub), to.plot$lb, to.plot$ub)
+    plot.d <- to.plot[, c("lambda", "v", "bound")]
 
     # find the location of x.names
-    location.beta <- to.plot$bound[to.plot$lambda==0]
-
-  }else if(lpv==3){
+    location.beta <- to.plot$bound[to.plot$lambda == 0]
+  } else if (lpv == 3) {
     plot.d <- to.plot
 
     # find the location of x.names
-    location.beta <- to.plot$pe[to.plot$lambda==0]
+    location.beta <- to.plot$pe[to.plot$lambda == 0]
   }
 
   # get the indices of selected variables
   selected.index <- x$var.index
-  black.index <- setdiff(1:p,selected.index)
+  black.index <- setdiff(1:p, selected.index)
 
   # change the color of the variables
-  color.use <- c(rep("black",length(black.index)),
-                 rep("blue",length(selected.index)))
+  color.use <- c(
+    rep("black", length(black.index)),
+    rep("blue", length(selected.index))
+  )
 
-  location.beta <- location.beta[c(black.index,selected.index)]
-  var.axis <- x.names[c(black.index,selected.index)]
+  location.beta <- location.beta[c(black.index, selected.index)]
+  var.axis <- x.names[c(black.index, selected.index)]
 
   # find the limit of the canvas
-  if(lpv==3){
-    ylim <- c(min(c(location.beta,to.plot$lb,to.plot$ub) )*1.01,
-              max(c(location.beta,to.plot$lb,to.plot$ub))*1.01)
+  if (lpv == 3) {
+    ylim <- c(
+      min(c(location.beta, to.plot$lb, to.plot$ub)) * 1.01,
+      max(c(location.beta, to.plot$lb, to.plot$ub)) * 1.01
+    )
 
-    ylim <- c(-max(abs(ylim)),max(abs(ylim)))
-    ytick <- c(-round(max(abs(ylim)),2),-round(max(abs(ylim)),2)/2,0,
-               round(max(abs(ylim)),2)/2,round(max(abs(ylim)),2) )
-  }else if (lpv==1){
-    ylim <- c(min(plot.d$bound) * 1.01,
-              max(plot.d$bound) * 1.01)
+    ylim <- c(-max(abs(ylim)), max(abs(ylim)))
+    ytick <- c(
+      -round(max(abs(ylim)), 2), -round(max(abs(ylim)), 2) / 2, 0,
+      round(max(abs(ylim)), 2) / 2, round(max(abs(ylim)), 2)
+    )
+  } else if (lpv == 1) {
+    ylim <- c(
+      min(plot.d$bound) * 1.01,
+      max(plot.d$bound) * 1.01
+    )
 
-    ylim <- c(-max(abs(ylim)),max(abs(ylim)))
-    ytick <- c(-round(max(abs(ylim)),2),-round(max(abs(ylim)),2)/2,0,
-               round(max(abs(ylim)),2)/2,round(max(abs(ylim)),2) )
-
+    ylim <- c(-max(abs(ylim)), max(abs(ylim)))
+    ytick <- c(
+      -round(max(abs(ylim)), 2), -round(max(abs(ylim)), 2) / 2, 0,
+      round(max(abs(ylim)), 2) / 2, round(max(abs(ylim)), 2)
+    )
   }
 
   # create a data set for the null bound
-  n.bound <- results[(3*p+1),]
+  n.bound <- results[(3 * p + 1), ]
 
   # find the lambda.1se
   vlambda <- x$lambda
 
   # plot the results
-  if(lpv==1){
+  if (lpv == 1) {
+    xvals <- split(plot.d$lambda, plot.d$v)
+    yvals <- split(plot.d$bound, plot.d$v)
 
-    xvals <- split(plot.d$lambda,plot.d$v)
-    yvals <- split(plot.d$bound,plot.d$v)
-
-    plot(lambda.seq,type="n",ylim=ylim,xlim=c(0,lambda.max),
-         xlab=expression(lambda),ylab="Bound that is closer to the null",
-         main="Solution to the two-stage algorithm with one line per variable",
-         axes=F,frame.plot=T)
-    axis(1,at=round(seq(0,lambda.max,length.out=5),3) )
-    axis(2,at=ytick,labels=c(ytick[1:2],0,ytick[4:5]))
-    mtext(var.axis,side=2,at=location.beta,col=color.use)
-    mtext(bquote(lambda["1se"]),side=1,at=vlambda)
-    polygon(c(lambda.seq,rev(lambda.seq)),c(-n.bound,rev(n.bound)),col="grey",border="grey")
-    invisible(mapply(lines,xvals,yvals,col=1:p))
-    abline(h=0)
-    abline(v=vlambda,lty=2)
-
-
-  }else if (lpv==3){
-
-    plot(lambda.seq,type="n",ylim=ylim,xlim=c(0,lambda.max),
-         xlab=expression(lambda),ylab="Point estimates and confidence intervals",
-         main="Solution to the two-stage algorithm with three lines per variable",
-         axes=F,frame.plot=T)
-    axis(1,at=round(seq(0,lambda.max,length.out=5),3) )
-    axis(2,at=ytick,labels=c(ytick[1:2],0,ytick[4:5]))
+    plot(lambda.seq,
+      type = "n", ylim = ylim, xlim = c(0, lambda.max),
+      xlab = expression(lambda), ylab = "Bound that is closer to the null",
+      main = "Solution to the two-stage algorithm with one line per variable",
+      axes = F, frame.plot = T
+    )
+    axis(1, at = round(seq(0, lambda.max, length.out = 5), 3))
+    axis(2, at = ytick, labels = c(ytick[1:2], 0, ytick[4:5]))
+    mtext(var.axis, side = 2, at = location.beta, col = color.use)
+    mtext(bquote(lambda["1se"]), side = 1, at = vlambda)
+    polygon(c(lambda.seq, rev(lambda.seq)), c(-n.bound, rev(n.bound)), col = "grey", border = "grey")
+    invisible(mapply(lines, xvals, yvals, col = 1:p))
+    abline(h = 0)
+    abline(v = vlambda, lty = 2)
+  } else if (lpv == 3) {
+    plot(lambda.seq,
+      type = "n", ylim = ylim, xlim = c(0, lambda.max),
+      xlab = expression(lambda), ylab = "Point estimates and confidence intervals",
+      main = "Solution to the two-stage algorithm with three lines per variable",
+      axes = F, frame.plot = T
+    )
+    axis(1, at = round(seq(0, lambda.max, length.out = 5), 3))
+    axis(2, at = ytick, labels = c(ytick[1:2], 0, ytick[4:5]))
 
     # null region
-    polygon(c(lambda.seq,rev(lambda.seq)),c(-n.bound,rev(n.bound)),col="grey",border="grey")
+    polygon(c(lambda.seq, rev(lambda.seq)), c(-n.bound, rev(n.bound)), col = "grey", border = "grey")
 
     # text on axis
-    mtext(var.axis,side=2,at=location.beta,col=color.use)
-    mtext(bquote(lambda["1se"]),side=1,at=vlambda)
+    mtext(var.axis, side = 2, at = location.beta, col = color.use)
+    mtext(bquote(lambda["1se"]), side = 1, at = vlambda)
 
     # point estimates
-    xvals <- split(plot.d$lambda,plot.d$v)
-    yvals <- split(plot.d$pe,plot.d$v)
-    invisible(mapply(lines,xvals,yvals,col=1:p))
+    xvals <- split(plot.d$lambda, plot.d$v)
+    yvals <- split(plot.d$pe, plot.d$v)
+    invisible(mapply(lines, xvals, yvals, col = 1:p))
 
     # upper bound
-    xvals <- split(plot.d$lambda,plot.d$v)
-    yvals <- split(plot.d$ub,plot.d$v)
-    invisible(mapply(lines,xvals,yvals,col=1:p,lty=2))
+    xvals <- split(plot.d$lambda, plot.d$v)
+    yvals <- split(plot.d$ub, plot.d$v)
+    invisible(mapply(lines, xvals, yvals, col = 1:p, lty = 2))
 
     # lower bound
-    xvals <- split(plot.d$lambda,plot.d$v)
-    yvals <- split(plot.d$lb,plot.d$v)
-    invisible(mapply(lines,xvals,yvals,col=1:p,lty=2))
+    xvals <- split(plot.d$lambda, plot.d$v)
+    yvals <- split(plot.d$lb, plot.d$v)
+    invisible(mapply(lines, xvals, yvals, col = 1:p, lty = 2))
 
-    abline(h=0)
-    abline(v=vlambda,lty=2)
-
+    abline(h = 0)
+    abline(v = vlambda, lty = 2)
   }
-
 }
-
--- R/utils.R
+++ R/utils.R
@@ -9,25 +9,24 @@
 #'
 #' @return Indices of variables selected
 
-get.var <- function(candidate.index,xs,ys){
-
-  if(length(candidate.index)==0){
+get.var <- function(candidate.index, xs, ys) {
+  if (length(candidate.index) == 0) {
     out.sgpv <- integer(0)
-  }else{
+  } else {
 
     # run fully relaxed LASSO
-    f.l <- lm(ys~xs[,candidate.index])
+    f.l <- lm(ys ~ xs[, candidate.index])
 
     # get confidence bands
-    pe <- summary(f.l)$coef[-1,1]
-    se <- summary(f.l)$coef[-1,2]
+    pe <- summary(f.l)$coef[-1, 1]
+    se <- summary(f.l)$coef[-1, 2]
     null.bound.p <- mean(se)
 
     # screen variables
-    out.sgpv <- candidate.index[which(abs(pe)> 1.96*se+null.bound.p )]
+    out.sgpv <- candidate.index[which(abs(pe) > 1.96 * se + null.bound.p)]
   }
 
-  return( out.sgpv)
+  return(out.sgpv)
 }
 
 
@@ -43,57 +42,53 @@ get.var <- function(candidate.index,xs,ys){
 #'
 #' @return A vector that contains the point estimates, confidence intervals and the null bound
 
-get.coef <- function(xs,ys,lambda,lasso){
-
+get.coef <- function(xs, ys, lambda, lasso) {
   p <- ncol(xs)
 
   # evaluate lasso at lambda
-  index <- which(coef(lasso,s=lambda)[-1] != 0)
+  index <- which(coef(lasso, s = lambda)[-1] != 0)
 
   # define the output
   out.coef <- numeric(p)
   out.lb <- numeric(p)
   out.ub <- numeric(p)
 
-  if(lambda==0){
+  if (lambda == 0) {
 
     # full ols model
-    full.ols <- lm(ys~xs)
+    full.ols <- lm(ys ~ xs)
 
-    pe <- summary(full.ols)$coef[-1,1]
-    se <- summary(full.ols)$coef[-1,2]
-    lb <- pe - 1.96*se
-    ub <- pe + 1.96*se
+    pe <- summary(full.ols)$coef[-1, 1]
+    se <- summary(full.ols)$coef[-1, 2]
+    lb <- pe - 1.96 * se
+    ub <- pe + 1.96 * se
 
-    null.bound.lasso <- mean(summary(full.ols)$coef[-1,2])
+    null.bound.lasso <- mean(summary(full.ols)$coef[-1, 2])
 
     out.coef <- as.numeric(pe)
     out.lb <- as.numeric(lb)
     out.ub <- as.numeric(ub)
-
-  }else if(length(index)!=0) {
+  } else if (length(index) != 0) {
 
     # run fully relaxed LASSO
-    f.l <- lm(ys~xs[,index])
+    f.l <- lm(ys ~ xs[, index])
 
     # get confidence bands
-    pe <- summary(f.l)$coef[-1,1]
-    se <- summary(f.l)$coef[-1,2]
-    lb <- pe - 1.96*se
-    ub <- pe + 1.96*se
+    pe <- summary(f.l)$coef[-1, 1]
+    se <- summary(f.l)$coef[-1, 2]
+    lb <- pe - 1.96 * se
+    ub <- pe + 1.96 * se
 
-    null.bound.lasso <- mean(summary(f.l)$coef[-1,2])
+    null.bound.lasso <- mean(summary(f.l)$coef[-1, 2])
 
     out.coef[index] <- as.numeric(pe)
     out.lb[index] <- as.numeric(lb)
     out.ub[index] <- as.numeric(ub)
-
-  }else if(length(index)==0){
+  } else if (length(index) == 0) {
 
     # intercept only model
     null.bound.lasso <- 0
-
   }
 
-  return(c(out.coef,out.lb,out.ub,null.bound.lasso))
+  return(c(out.coef, out.lb, out.ub, null.bound.lasso))
 }
